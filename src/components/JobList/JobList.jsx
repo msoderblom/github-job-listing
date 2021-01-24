aa5889919c4eb9ce560e6dcd5a50d8fa34d5cbdf
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-01-24 17:52:08 +0100

    Change & style JobList

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import JobItem from "../JobItem";
 import * as S from "./styled";
 
 const JobList = ({ searchResults }) => {
+  const history = useHistory();
+
+  // const handleOnClick = () => {
+  //   history.push(`/jobs/${job.id}`);
+  // };
   return (
     <S.Container>
       {searchResults.length !== 0 ? (
@@ -10,7 +16,9 @@ const JobList = ({ searchResults }) => {
           <h2>Search results</h2>
           <S.List>
             {searchResults.map((job) => (
-              <JobItem key={job.id} job={job} />
+              <li key={job.id} onClick={() => history.push(`/jobs/${job.id}`)}>
+                <JobItem job={job} />
+              </li>
             ))}
           </S.List>
         </>
--- src/components/JobList/styled.js
+++ src/components/JobList/styled.js
@@ -1,18 +1,36 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  h2 {
+  padding-inline: 10px;
+  min-width: 0;
+
+  & > h2 {
     color: #fcfbfc;
   }
 `;
 export const List = styled.ul`
   list-style: none;
   padding: 0;
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  gap: 20px;
+
+  // Medium devices (tablets, 768px and up)
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  // Large devices (desktops, 992px and up)
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+
+  // Extra large devices (large desktops, 1200px and up)
+  /*  @media (min-width: 1200px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  } */
   li {
-    min-width: 350px;
-    max-width: 20%;
-    margin-right: 10px;
+    display: flex;
+    align-items: stretch;
+    cursor: pointer;
   }
 `;
